bc57367a11a97932e2318f48956fe6f4fafc3ed0
Aldana Maguna <aldumaguna@gmail.com>
2026-06-11 19:50:49 -0300

    Responsive

--- js/main.js
+++ js/main.js
@@ -217,3 +217,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
